2ba605190d38fdf569af2030099c74e7825085e7
Tom Schmidt <tschmidt.neo@googlemail.com>
2016-02-10 20:22:41 +0100

    start change code to swift

---
+++ WhatsMac/WAMApplication.swift
@@ -0,0 +1,32 @@
+//
+// Created by Tom Schmidt on 10.02.16.
+// Copyright (c) 2016 Tom Schmidt. All rights reserved.
+//
+import Foundation
+import AppKit
+
+class WAMApplication : NSApplication {
+    override func sendEvent(theEvent: NSEvent) {
+        let appDelegate = self.delegate as! AppDelegate
+        
+        if theEvent.type == .LeftMouseUp {
+            appDelegate.shouldPropagateMouseUpEvent(theEvent)
+            super.sendEvent(theEvent)
+            return
+        }
+        if theEvent.type == .LeftMouseDragged {
+            appDelegate.shouldPropagateMouseDraggedEvent(theEvent)
+            super.sendEvent(theEvent)
+            return
+        }
+        if theEvent.type == .KeyDown && theEvent.modifierFlags.contains(.CommandKeyMask) {
+            switch theEvent.characters! {
+                case "1", "2", "3", "4", "5", "6", "7", "8", "9":
+                    appDelegate.setActiveConversationAtIndex(theEvent.characters)
+                    return
+                default: break
+            }
+        }
+        super.sendEvent(theEvent)
+    }
+}
---
+++ WhatsMac/WAMWebView.swift
@@ -0,0 +1,19 @@
+//
+// Created by Tom Schmidt on 10.02.16.
+// Copyright (c) 2016 Tom Schmidt. All rights reserved.
+//
+
+import Foundation
+import WebKit
+
+class WAMWebView : WKWebView {
+    override func performKeyEquivalent(theEvent:NSEvent) -> Bool {
+        let result: Bool = super.performKeyEquivalent(theEvent)
+        switch theEvent.keyCode {
+        case 53, 123, 124:
+            return true;
+        default:
+            return result;
+        }
+    }
+}
